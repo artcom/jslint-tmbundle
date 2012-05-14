5d62c958f15bedf24770116ce63a86f1f9e29ac9
Andreas Marr <andreasm@artcom.de>
2012-05-14 14:44:07 +0200

    support for passing in the runtime spec in the jslinter.
    Not pretty and using options would be much better here.
    Also jslint class should not validate immediately on initialization.

--- spec/scripts/jslint_spec.rb
+++ spec/scripts/jslint_spec.rb
@@ -3,22 +3,29 @@ require 'spec_helper'
 require 'jslint'
 
 describe "JsLint" do
-
+  
+  before(:each) do
+    @runtimeSpec = JSLINT::RuntimeSpecs::JSC
+    if ENV['js_runtime'] == "rhino"
+      @runtimeSpec = JSLINT::RuntimeSpecs::RHINO
+    end
+  end
+  
   it "can be instantiated" do
-    myJsLint = JsLint.new "#{RSpec.configuration.fixtures}/valid_min.js"#, ".jslintrc"
+    myJsLint = JsLint.new "#{RSpec.configuration.fixtures}/valid_min.js", ".jslintrc", @runtimeSpec
     
     myJsLint.jslint_result.should_not be_nil
   end
   
   it "can validate a valid javascript" do
-    myJsLint = JsLint.new "#{RSpec.configuration.fixtures}/valid_min.js", ".jslintrc"
+    myJsLint = JsLint.new "#{RSpec.configuration.fixtures}/valid_min.js", ".jslintrc", @runtimeSpec
     
     myJsLint.jslint_result.should be_a_kind_of(Hash)
     myJsLint.jslint_result["result"].should be_true
   end
 
   it "can report errors on invalid javascript" do
-    myJsLint = JsLint.new "#{RSpec.configuration.fixtures}/invalid.js", ".jslintrc"
+    myJsLint = JsLint.new "#{RSpec.configuration.fixtures}/invalid.js", ".jslintrc", @runtimeSpec
     
     myJsLint.jslint_result["result"].should be_false
     myJsLint.jslint_result['data']['errors'].should have(5).items
