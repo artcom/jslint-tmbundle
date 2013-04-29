e8da79d9f2cbe6e8fd6c9d0b8cdae6289350aef5
Andreas Marr <andreasm@artcom.de>
2013-04-29 16:52:31 +0200

    Updated to current jslint (patched with infixin option) due to errors in Safari 6 environment.

--- Support/javascripts/jslint_support.js
+++ Support/javascripts/jslint_support.js
@@ -1,7 +1,7 @@
 /*globals JSLINT, print, quit */
 
 (function (a) {
-    var i, raw_data;
+    var i, raw_data, option;
     if (!a[0]) {
         print('Usage: jslint.js -- "[your_javascript]" "[your_options]"');
         quit(1);
@@ -10,7 +10,15 @@
     if (a[1]) {
         options = JSON.parse(a[1]);
     }
-    
+    for (option in options) {
+        if (options[option] === "true") {
+            options[option] = true;
+        } else {
+            options[option] = false;
+        }
+    }
+
+
     var my_result = JSLINT(a[0], options);
     try {
         raw_data = JSLINT.data();
@@ -20,6 +28,8 @@
                 delete raw_data.functions[i].params;
             }
         }
+        raw_data.tokens = undefined;
+
         print(JSON.stringify({data: raw_data, result: my_result}));
     } catch (e) {
         print(e);
