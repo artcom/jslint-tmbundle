b35cbe1b697d4ff1f2acc06ce7b0c73ebdea036e
Andreas Marr <andreasm@artcom.de>
2013-05-15 14:36:43 +0200

    excluding spec/* from coverage report.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,17 @@
 require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
 SimpleCov.start do
   add_filter "/spec/"
-end if ENV["COVERAGE"]
+end
+
+#Coveralls.wear!
 
 $:.unshift File.expand_path("../../Support/scripts", __FILE__)
 
 # Simulate Textmate environment
 ENV['TM_BUNDLE_SUPPORT'] = File.expand_path("../../Support", __FILE__)
-
-require 'coveralls'
-Coveralls.wear!
 
 RSpec.configure do |config|
   config.add_setting(
